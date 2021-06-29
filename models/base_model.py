d086ea899c3cce8f48ce8dcd920c48c120c12794
gnnrbachelor <gnnrbachelor@gmail.com>
2021-06-29 18:42:39 +0000

    test base_model change

--- models/base_model.py
+++ models/base_model.py
@@ -26,8 +26,8 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
             models.storage.new(self)
             models.storage.save()
 
