d23ceec6fa484844e0dd3d1f3e1bdcf48d1fe7a3
gnnrbachelor <gnnrbachelor@gmail.com>
2021-06-29 13:38:27 +0000

    Add console.py

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""console module"""
+
+import cmd
+from datetime import datetime
+import models
+from models.base_model import BaseModel
+import shlex
+
+class HBNBCommand(cmd.Cmd):
+    """HBNBCommand class for prompt"""
+    prompt = '(hbnb) '
+
+    def do_EOF(self, arg):
+        """Exit the console"""
+        return True
+
+    def do_quit(self, arg):
+        """Quit command"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
